76c91dbfe68cf0cf90b8ed2ab6c5aa12ce07bc23
numancan <n.canipekci@gmail.com>
2022-10-24 20:18:26 +0300

    Added multicore

--- src/main.c
+++ src/main.c
@@ -6,17 +6,28 @@
 
 #include "pico/multicore.h"
 
+/* Core 1 main function */
+void main1(void)
+{
+
+}
+
+/* Core 0 main function */
 int main(void)
 {
   stdio_init_all();
+
+  multicore_launch_core1(main1);
   hid_init();
   
   while (1)
   {
+    /* HID tasks */
     tud_task(); // tinyusb device task
     led_blinking_task();
-
     hid_task();
+
+
   }
 
   return 0;
