4773cd1bddb4af0fb3a8bb8195a1baf9be7fc09d
numancan <n.canipekci@gmail.com>
2026-05-25 16:22:34 +0300

    Added automatic init structure for task manager

--- src/app/task_manager/task_manager.c
+++ src/app/task_manager/task_manager.c
@@ -5,7 +5,7 @@
 // Headers for task functions
 #include "app/duckyscript/ducky.h"
 #include "app/gui/gui.h"
-#include "app/hid/keyboard.h"
+#include "app/hid/usb_device.h"
 #include "app/status_led/status_led.h"
 #include "app/web_server/web_server.h"
 #include "middleware/input.h"
@@ -16,21 +16,26 @@ typedef struct {
     uint32_t stack_size;
     UBaseType_t default_prio;
     TaskHandle_t handle;
-    bool notify_on_stop;  // If true, send notification instead of vTaskDelete
+    bool notify_on_stop;
+    void (*init_func)(void);  // Called once before task starts, may be NULL
 } task_config_t;
 
 static task_config_t tasks[TASK_MANAGER_TASK_COUNT] = {
-    [TASK_MANAGER_TASK_GUI] = {gui_task, "gui", 1024, tskIDLE_PRIORITY + 1, NULL, true},
-    [TASK_MANAGER_TASK_INPUT] = {input_task, "input", 512, tskIDLE_PRIORITY + 2, NULL, true},
-    [TASK_MANAGER_TASK_WEB_SERVER] = {web_server_task, "webserver", 1024, tskIDLE_PRIORITY + 1, NULL, true},
-    [TASK_MANAGER_TASK_DUCKY] = {ducky_task, "ducky", 1024, tskIDLE_PRIORITY + 1, NULL, true},
-    [TASK_MANAGER_TASK_USB_HID] = {usb_hid_task, "usb_hid", 1024, tskIDLE_PRIORITY + 3, NULL, false},
-    [TASK_MANAGER_TASK_STATUS_LED] = {status_led_task, "status_led", 512, tskIDLE_PRIORITY + 3, NULL, false},
+    [TASK_MANAGER_TASK_USB_HID] = {usb_device_task, "usb_device", 1024, configMAX_PRIORITIES - 1, NULL, false, usb_device_init},
+    [TASK_MANAGER_TASK_INPUT] = {input_task, "input", 512, tskIDLE_PRIORITY + 4, NULL, true, input_init},
+    [TASK_MANAGER_TASK_DUCKY] = {ducky_task, "ducky", 1024, tskIDLE_PRIORITY + 3, NULL, true, ducky_init},
+    [TASK_MANAGER_TASK_WEB_SERVER] = {web_server_task, "webserver", 1024, tskIDLE_PRIORITY + 2, NULL, true, NULL},
+    [TASK_MANAGER_TASK_GUI] = {gui_task, "gui", 1024, tskIDLE_PRIORITY + 2, NULL, true, NULL},
+    [TASK_MANAGER_TASK_STATUS_LED] = {status_led_task, "status_led", 512, tskIDLE_PRIORITY + 1, NULL, false, NULL},
 };
 
 bool task_manager_start(TaskManagerId id) {
     if (id >= TASK_MANAGER_TASK_COUNT) return false;
     if (tasks[id].handle != NULL) return true;  // Already running
+
+    if (tasks[id].init_func != NULL) {
+        tasks[id].init_func();
+    }
 
     BaseType_t ret = xTaskCreate(tasks[id].func, tasks[id].name, tasks[id].stack_size,
                                  (void*)(uintptr_t)id,  // Pass ID as parameter if needed
