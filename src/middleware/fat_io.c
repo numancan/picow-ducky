40000f3636bf7787f462b5c84d02c169b1138fc7
numancan <n.canipekci@gmail.com>
2026-05-28 00:41:58 +0300

    refactor: rename sd_card to fat_io

--- src/middleware/fat_io.c
+++ src/middleware/fat_io.c
@@ -1,94 +1,93 @@
-#include "sd_card.h"
+#include "fat_io.h"
 
 #include "hw_config.h"
-#include "middleware/debug.h"
 
-static bool sd_is_mounted = false;
+static bool fat_io_is_mounted_flag = false;
 
-FRESULT sd_card_mount() {
+FRESULT fat_io_mount() {
     sd_card_t* sd_card = sd_get_by_num(0);
 
-    PANIC_IF("SD CARD not initiated!", sd_card->state.m_Status == STA_NOINIT);
+    // PANIC_IF("SD CARD not initiated!", sd_card->state.m_Status == STA_NOINIT);
 
     FRESULT fr = f_mount(&sd_card->state.fatfs, "", 1);
 
-    DEBUG_PRINTF("sdm_mount: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("fat_io_mount: %s (%d)\n", FRESULT_str(fr), fr);
 
-    if (fr == FR_OK) sd_is_mounted = true;
+    if (fr == FR_OK) fat_io_is_mounted_flag = true;
     return fr;
 }
 
-FRESULT sd_card_unmount() {
+FRESULT fat_io_unmount() {
     FRESULT fr = f_unmount("");
 
-    DEBUG_PRINTF("sdm_unmount: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("fat_io_unmount: %s (%d)\n", FRESULT_str(fr), fr);
 
-    if (fr == FR_OK) sd_is_mounted = false;
+    if (fr == FR_OK) fat_io_is_mounted_flag = false;
     return fr;
 }
 
-bool sd_card_is_mounted() { return sd_is_mounted; }
+bool fat_io_is_mounted() { return fat_io_is_mounted_flag; }
 
-FRESULT sd_card_open_write(FIL* fil, const char* const filename) {
+FRESULT fat_io_open_write(FIL* fil, const char* const filename) {
     FRESULT fr = f_open(fil, filename, FA_OPEN_APPEND | FA_WRITE | FA_CREATE_ALWAYS);
 
-    DEBUG_PRINTF("f_open(%s): %s (%d)\n", filename, FRESULT_str(fr), fr);
+    printf("f_open(%s): %s (%d)\n", filename, FRESULT_str(fr), fr);
 
     return fr;
 }
 
-FRESULT sd_card_open_read(FIL* fil, const char* const filename) {
+FRESULT fat_io_open_read(FIL* fil, const char* const filename) {
     FRESULT fr = f_open(fil, filename, FA_READ);
 
     // TCHAR str[126];
     // fr = f_getcwd(str, 126); /* Get current directory path */
     // printf("%s\n", fr);
 
-    DEBUG_PRINTF("sd_card_open: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("fat_io_open_read: %s (%d)\n", FRESULT_str(fr), fr);
 
     return fr;
 }
 
-FRESULT sd_card_close(FIL* fil) {
+FRESULT fat_io_close(FIL* fil) {
     FRESULT fr = f_close(fil);
 
-    DEBUG_PRINTF("f_close: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("f_close: %s (%d)\n", FRESULT_str(fr), fr);
 
     return fr;
 }
 
-sd_read_result_t sd_card_read_line(FIL* file, char* buf, size_t buf_size) {
+FatIoReadResult fat_io_read_line(FIL* file, char* buf, size_t buf_size) {
     if (!f_gets(buf, buf_size, file)) {
-        return f_eof(file) ? SD_READ_EOF : SD_READ_ERROR;
+        return f_eof(file) ? FAT_IO_READ_EOF : FAT_IO_READ_ERROR;
     }
 
     size_t len = strlen(buf);
     bool has_newline = (len > 0) && (buf[len - 1] == '\n' || buf[len - 1] == '\r');
 
     if (!has_newline && !f_eof(file)) {
-        return SD_READ_LINE_TOO_LONG;
+        return FAT_IO_READ_LINE_TOO_LONG;
     }
 
-    return SD_READ_OK;
+    return FAT_IO_READ_OK;
 }
 
-FRESULT sd_card_write(FIL* fil, char* str, uint32_t size) {
+FRESULT fat_io_write(FIL* fil, char* str, uint32_t size) {
     UINT written_byte_size;
 
     FRESULT fr = f_write(fil, str, size, &written_byte_size);
 
     // TODO: check size == writen
     if (fr != FR_OK && written_byte_size < 0) {
-        DEBUG_PRINTF("ERROR: Could not write to file (%d)\r\n", written_byte_size);
+        printf("ERROR: Could not write to file (%d)\r\n", written_byte_size);
         return fr;
     }
 
-    DEBUG_PRINTF("SC written byte:%lu == %lu\n", written_byte_size, size);
+    printf("fat_io written byte:%lu == %lu\n", written_byte_size, size);
 
     return fr;
 }
 
-uint32_t sd_card_list_dir(const char* path, char* file_list, uint32_t n, const char* delim) {
+uint32_t fat_io_list_dir(const char* path, char* file_list, uint32_t n, const char* delim) {
     FRESULT fr;
     DIR dir;
     FILINFO fno;
@@ -104,13 +103,13 @@ uint32_t sd_card_list_dir(const char* path, char* file_list, uint32_t n, const c
             if (fr != FR_OK || fno.fname[0] == 0) break;
 
             if (fno.fattrib & AM_DIR) { /* Directory */
-                DEBUG_PRINTF("   <DIR>   %s\n", fno.fname);
+                printf("   <DIR>   %s\n", fno.fname);
             } else { /* File */
-                // DEBUG_PRINTF("%s\n", fno.fname);
+                // printf("%s\n", fno.fname);
 
                 size_t fname_size = strlen(fno.fname);
                 if (!(n > fname_size)) {
-                    printf("SC: There is no space to write file list");
+                    printf("fat_io: There is no space to write file list");
                     fr = FR_INVALID_PARAMETER;
                     break;
                 }
@@ -121,7 +120,7 @@ uint32_t sd_card_list_dir(const char* path, char* file_list, uint32_t n, const c
                 n -= fname_size + 1;  // +1 for delim
             }
         }
-        DEBUG_PRINTF("sd_card_list_dir: %d file found!\n", nFile);
+        printf("fat_io_list_dir: %d file found!\n", nFile);
     } else {
         printf("Failed to open \"%s\". (%u)\n", path, fr);
     }
@@ -131,7 +130,7 @@ uint32_t sd_card_list_dir(const char* path, char* file_list, uint32_t n, const c
     return nFile;
 }
 
-uint32_t sd_card_count_dir(const char* path) {
+uint32_t fat_io_count_dir(const char* path) {
     FRESULT fr;
     DIR dir;
     FILINFO fno;
@@ -151,7 +150,7 @@ uint32_t sd_card_count_dir(const char* path) {
     return nFile;
 }
 
-FRESULT sd_card_read_until(FIL* fil, char* buffer, char delim, int16_t len) {
+FRESULT fat_io_read_until(FIL* fil, char* buffer, char delim, int16_t len) {
     char s[2];
     UINT br;
     size_t i = 0;
@@ -171,20 +170,20 @@ FRESULT sd_card_read_until(FIL* fil, char* buffer, char delim, int16_t len) {
     return FR_OK;
 }
 
-FRESULT sd_card_remove_file(const char* filename) {
+FRESULT fat_io_remove_file(const char* filename) {
     FRESULT fr = f_unlink(filename);
 
-    DEBUG_PRINTF("sd_card_remove_file: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("fat_io_remove_file: %s (%d)\n", FRESULT_str(fr), fr);
     return fr;
 }
 
-FRESULT sd_card_check_file_exits(const char* fname) {
+FRESULT fat_io_check_file_exits(const char* fname) {
     FRESULT fr;
     FILINFO fno;
-    DEBUG_PRINTF("Test for \"%s\"...\n", fname);
+    printf("Test for \"%s\"...\n", fname);
 
     fr = f_stat(fname, &fno);
 
-    DEBUG_PRINTF("sd_card_check_file_exits: %s (%d)\n", FRESULT_str(fr), fr);
+    printf("fat_io_check_file_exits: %s (%d)\n", FRESULT_str(fr), fr);
     return fr;
 }
