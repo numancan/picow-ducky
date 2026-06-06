fb6976805d1def922e5e91a9b0812cb7f1152042
numancan <n.canipekci@gmail.com>
2026-06-06 22:41:44 +0300

    refactor(cgi): remove pub/sub decoupling and trigger actions directly

--- src/app/web_server/cgi.c
+++ src/app/web_server/cgi.c
@@ -1,6 +1,7 @@
 #include "cgi.h"
 
-#include "app/pubsub/pubsub.h"
+#include "app/duckyscript/ducky.h"
+#include "app/settings/settings.h"
 #include "lwip/apps/httpd.h"
 #include "lwip/def.h"
 #include "lwip/init.h"
@@ -10,27 +11,27 @@
 
 static const char* TAG = "CGI";
 
-static PubSub cgi_pubsub = {.subs_callbacks = NULL, .subscribe_count = 0};
+typedef enum { CGI_EVENT_NONE = 0, CGI_EVENT_TRIGGER, CGI_EVENT_DELETE, CGI_EVENT_SETTINGS } CgiEventType;
+
+static CgiEventType cgi_event_type_from_str(const char* str) {
+    if (strcmp(str, "trigger") == 0) return CGI_EVENT_TRIGGER;
+    if (strcmp(str, "delete") == 0) return CGI_EVENT_DELETE;
+    if (strcmp(str, "settings") == 0) return CGI_EVENT_SETTINGS;
+    return CGI_EVENT_NONE;
+}
 
 static const char* cgi_handler(int iIndex, int iNumParams, char* pcParam[], char* pcValue[]) {
-    CgiEvent cgi_event = {0, NULL};
+    if (iNumParams < 1) return "/index.shtml";
 
     LOG_INFO(TAG, "CGI HANDLER: %s %s", pcParam[0], pcValue[0]);
 
-    // TODO: can make without if else
-    if (!strcmp(pcParam[0], "trigger")) {
-        cgi_event.cgi_event_type = CGI_EVENT_TRIGGER;
-        cgi_event.value = pcValue[0];
-
-    } else if (strcmp(pcParam[0], "delete") == 0) {
-        cgi_event.cgi_event_type = CGI_EVENT_DELETE;
-        cgi_event.value = pcValue[0];
-
-    } else if (strcmp(pcParam[0], "settings") == 0) {
-        cgi_event.cgi_event_type = CGI_EVENT_SETTINGS;
+    // TODO: lwIP TCP thread blocking here not good idea
+    switch (cgi_event_type_from_str(pcParam[0])) {
+        case CGI_EVENT_TRIGGER: ducky_play_script(pcValue[0]); break;
+        case CGI_EVENT_DELETE: break;  // TODO: payload delete
+        case CGI_EVENT_SETTINGS: settings_load_from_file(); break;
+        case CGI_EVENT_NONE: break;
     }
-
-    if (cgi_event.cgi_event_type) pubsub_notify(&cgi_pubsub, &cgi_event);
 
     return "/index.shtml";
 }
@@ -40,11 +41,4 @@ static tCGI cgi_handlers[] = {
     {"/index.shtml", cgi_handler},
 };
 
-void cgi_event_subscribe(CgiCallback callback) { pubsub_subscribe(&cgi_pubsub, callback); }
-
-void cgi_init() {
-    // cgi_pubsub = pubsub_alloc();
-    http_set_cgi_handlers(cgi_handlers, LWIP_ARRAYSIZE(cgi_handlers));
-}
-
-void cgi_deinit() {}
+void cgi_init() { http_set_cgi_handlers(cgi_handlers, LWIP_ARRAYSIZE(cgi_handlers)); }
