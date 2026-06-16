23cd0b26ec428a888afd2451125fe10ecf3eb55c
numancan <n.canipekci@gmail.com>
2026-06-17 00:11:35 +0300

    feat(input): implement button auto-repeat

--- src/middleware/input.c
+++ src/middleware/input.c
@@ -1,28 +1,34 @@
 #include "input.h"
 
 #include "FreeRTOS.h"
+#include "app/pubsub/pubsub.h"
 #include "app/task_manager/task_manager.h"
 #include "hal/hal.h"
 #include "hal/hal_gpio.h"
 #include "log.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "sys_fault.h"
 #include "task.h"
 #include "timers.h"
 
 #define DEBOUNCE_MS (50)
-#define LONG_PRESS_MS (800)
+#define LONG_PRESS_MS (600)
+#define REPEAT_DELAY_MS (1200)
+#define REPEAT_INTERVAL_MS (200)
 #define TIMER_PERIOD_MS (20)
+#define INPUT_PUBSUB_MAX_SUBS (3)
 
 typedef struct {
     GpioInput* gpio;
-    TimerHandle_t timer_handler;
+    TimerHandle_t timer_handle;
     volatile uint16_t press_count_ms;
+    volatile uint16_t last_repeat_ms;
 } InputState;
 
 typedef struct {
     InputState* states;
-    PubSubFree* pubsub;
+    PubSub* pubsub;
 } Input;
 
 static const char* TAG = "INPUT";
@@ -30,10 +36,8 @@ static const char* TAG = "INPUT";
 static Input* input = NULL;
 
 static void input_timer_callback(TimerHandle_t xTimer) {
-    uint8_t timer_id = (uint8_t)pvTimerGetTimerID(xTimer);
+    size_t timer_id = (size_t)pvTimerGetTimerID(xTimer);
     input->states[timer_id].press_count_ms += TIMER_PERIOD_MS;
-    // printf("ID:%d press_ms: %d\n",  (size_t) pvTimerGetTimerID( xTimer ),
-    // inputs[timer_id].press_count_ms);
 }
 
 const char* input_event_get_name(InputEventType event_type) {
@@ -42,9 +46,9 @@ const char* input_event_get_name(InputEventType event_type) {
 
         case INPUT_EVENT_TYPE_LONG_PRESS: return "Long"; break;
 
-        case INPUT_EVENT_TYPE_RELEASE: return "Release"; break;
+        case INPUT_EVENT_TYPE_REPEAT: return "Repeat"; break;
 
-        default: return "wtf?"; break;
+        default: return ""; break;
     }
 }
 
@@ -58,17 +62,21 @@ static InputEventType input_find_event_type(uint16_t press_count_ms) {
     return INPUT_EVENT_TYPE_NONE;
 }
 
-PubSubFree* input_get_pubsub() { return input->pubsub; }
+QueueHandle_t input_subscribe(uint8_t depth) { return pubsub_subscribe(input->pubsub, depth); }
+
+void input_unsubscribe(QueueHandle_t queue) { pubsub_unsubscribe(input->pubsub, queue); }
 
 void input_init() {
     input = (Input*)malloc(sizeof(Input));
     input->states = (InputState*)malloc(sizeof(InputState) * gpio_input_count);
-    input->pubsub = pubsub_free_alloc();
+    input->pubsub = pubsub_alloc(sizeof(InputEvent), INPUT_PUBSUB_MAX_SUBS);
+    PANIC_IF(input->pubsub == NULL, "pubsub alloc failed");
 
     for (size_t i = 0; i < gpio_input_count; i++) {
         input->states[i].gpio = (GpioInput*)&gpio_inputs[i];
         input->states[i].press_count_ms = 0;
-        input->states[i].timer_handler =
+        input->states[i].last_repeat_ms = 0;
+        input->states[i].timer_handle =
             xTimerCreate("TM_IN", pdMS_TO_TICKS(TIMER_PERIOD_MS), pdTRUE, (void*)i, input_timer_callback);
     }
 }
@@ -79,24 +87,41 @@ void input_task(void* params) {
             InputState input_pin = input->states[i];
             uint8_t pin_state = hal_gpio_read(input_pin.gpio->pin);
 
-            // Key pressed
-            if (pin_state != input_pin.gpio->default_state && xTimerIsTimerActive(input_pin.timer_handler) == pdFALSE) {
-                xTimerStart(input_pin.timer_handler, 0);
-            } else if (pin_state == input_pin.gpio->default_state && xTimerIsTimerActive(input_pin.timer_handler)) {
-                xTimerStop(input_pin.timer_handler, 0);
-
-                InputEvent input_event;
-                input_event.type = input_find_event_type(input_pin.press_count_ms);
-
-                if (input_event.type != INPUT_EVENT_TYPE_NONE) {
+            if (pin_state != input_pin.gpio->default_state && xTimerIsTimerActive(input_pin.timer_handle) == pdFALSE) {
+                xTimerStart(input_pin.timer_handle, 0);
+            } else if (pin_state != input_pin.gpio->default_state && xTimerIsTimerActive(input_pin.timer_handle)) {
+                // Key held: emit a REPEAT every interval once the initial delay has elapsed
+                if (input_pin.press_count_ms >= REPEAT_DELAY_MS &&
+                    (uint16_t)(input_pin.press_count_ms - input_pin.last_repeat_ms) >= REPEAT_INTERVAL_MS) {
+                    InputEvent input_event;
+                    input_event.type = INPUT_EVENT_TYPE_REPEAT;
                     input_event.key = input_pin.gpio->key;
-                    pubsub_free_notify(input->pubsub, &input_event);
+                    pubsub_notify(input->pubsub, &input_event);
 
-                    LOG_DEBUG(TAG, "Buton Key: %d - Input Event: %s - Press(ms): %d", input_pin.gpio->key,
+                    input->states[i].last_repeat_ms = input_pin.press_count_ms;
+
+                    LOG_DEBUG(TAG, "Button Key: %d - Input Event: %s - Press(ms): %d", input_pin.gpio->key,
                               input_event_get_name(input_event.type), input_pin.press_count_ms);
+                }
+            } else if (pin_state == input_pin.gpio->default_state && xTimerIsTimerActive(input_pin.timer_handle)) {
+                xTimerStop(input_pin.timer_handle, 0);
+
+                // Suppress the release event if the press already turned into a repeat (no trailing LONG_PRESS)
+                if (input_pin.last_repeat_ms == 0) {
+                    InputEvent input_event;
+                    input_event.type = input_find_event_type(input_pin.press_count_ms);
+
+                    if (input_event.type != INPUT_EVENT_TYPE_NONE) {
+                        input_event.key = input_pin.gpio->key;
+                        pubsub_notify(input->pubsub, &input_event);
+
+                        LOG_DEBUG(TAG, "Button Key: %d - Input Event: %s - Press(ms): %d", input_pin.gpio->key,
+                                  input_event_get_name(input_event.type), input_pin.press_count_ms);
+                    }
                 }
 
                 input->states[i].press_count_ms = 0;
+                input->states[i].last_repeat_ms = 0;
             }
         }
 
@@ -108,12 +133,12 @@ void input_task(void* params) {
     // Cleanup
     if (input != NULL) {
         for (size_t i = 0; i < gpio_input_count; i++) {
-            if (input->states[i].timer_handler != NULL) {
-                xTimerStop(input->states[i].timer_handler, 0);
-                xTimerDelete(input->states[i].timer_handler, 0);
+            if (input->states[i].timer_handle != NULL) {
+                xTimerStop(input->states[i].timer_handle, 0);
+                xTimerDelete(input->states[i].timer_handle, 0);
             }
         }
-        pubsub_free_dealloc(input->pubsub);
+        pubsub_dealloc(input->pubsub);
         free(input->states);
         free(input);
         input = NULL;
--- src/middleware/input.h
+++ src/middleware/input.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "app/pubsub/pubsub_free.h"
+#include "FreeRTOS.h"
 #include "hal/hal.h"
 #include "hal/hal_gpio.h"
+#include "queue.h"
 
 typedef enum {
     INPUT_EVENT_TYPE_PRESS,
     INPUT_EVENT_TYPE_LONG_PRESS,
-    INPUT_EVENT_TYPE_DOUBLE_PRESS,
-    INPUT_EVENT_TYPE_RELEASE,
+    INPUT_EVENT_TYPE_REPEAT,
     INPUT_EVENT_TYPE_NONE
 } InputEventType;
 
@@ -20,4 +20,5 @@ typedef struct {
 void input_init();
 void input_task(void* params);
 const char* input_event_get_name(InputEventType);
-PubSubFree* input_get_pubsub();
+QueueHandle_t input_subscribe(uint8_t depth);
+void input_unsubscribe(QueueHandle_t queue);
